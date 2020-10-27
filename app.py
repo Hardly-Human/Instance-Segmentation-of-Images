918afefffef3c3d0eab0ff4e45fd9006ddadff5f
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-10-27 17:20:13 +0530

    Added Main function

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+from gluoncv import model_zoo, data, utils
+
+def main():
+  
+  st.title("Instance Segmentation App")
+  st.text("Built with gluoncv and Streamlit")
+  st.markdown("### [About Instance Segmentation]() `            ` [View Source]()")
+
+
+if __name__== "__main__":
+  main()
